ba25184df46f57215973d0ac802b63e0d9938301
nazzang49 <34739974+nazzang49@users.noreply.github.com>
2018-11-14 12:18:03 +0900

    https://www.acmicpc.net/problem/6581

---
+++ B_6581.java
@@ -0,0 +1,14 @@
+package test_181105;
+
+import java.util.Scanner;
+
+//HTML 소스로 입력된 값을 태그 적용하여 출력 값으로 변환
+public class B_6581 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+	}
+
+}
