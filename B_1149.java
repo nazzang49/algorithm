70d613a26c508f9fbeaa967f322ac88131fb15a5
nazzang49 <34739974+nazzang49@users.noreply.github.com>
2019-01-16 12:58:48 +0900

    https://www.acmicpc.net/problem/1149

--- B_1149.java
+++ B_1149.java
@@ -1,74 +1,36 @@
-package test_181204;
+package test_190116;
 
 import java.util.Scanner;
 
-//줄지어 선 모든 집들을 색칠할 때 이웃한 집끼리는 같은 색을 칠할 수 없다는 조건이 주어진다면 최소 비용 구하기
+//칠해야 하는 집의 개수 n, 빨강, 파랑, 초록 페인트칠 비용이 주어질 때 모든 집을 칠하는 비용의 최솟값 구하기
 public class B_1149 {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		//집의 개수
 		int n = sc.nextInt();
 		
 		if(1<=n&&n<=1000) {
-			//최소비용 누적 저장할 dp 배열
-			int [] dp = new int[n];
-			char [] house = new char[n];
-			//각 집을 빨강, 초록, 파랑으로 칠할 때 소요되는 비용 
-			int [] R = new int[n];
-			int [] G = new int[n];
-			int [] B = new int[n];
+			//2차원 배열로 색깔별 비용 입력
+			int [][] dp = new int[n][n];
 			for(int i=0;i<n;i++) {
-				//모든 비용은 1000보다 작음
-				R[i]=sc.nextInt();
-				G[i]=sc.nextInt();
-				B[i]=sc.nextInt();
-			}
-			int min = Integer.MAX_VALUE;
-			int k=0;
-			while(k<3) {
-				if(k==0) {
-					dp[0]=R[0];
-					house[0]='R';
-				}else if(k==1) {
-					dp[0]=G[0];
-					house[0]='G';
-				}else {
-					dp[0]=B[0];
-					house[0]='B';
+				for(int j=0;j<n;j++) {
+					dp[i][j]=sc.nextInt();
 				}
-				//두 번째부터 dp 적용
-				for(int i=1;i<n;i++) {
-					if(house[i-1]=='R') {
-						dp[i]=Math.min(dp[i-1]+G[i], dp[i-1]+B[i]);
-						if(dp[i]==dp[i-1]+G[i]) {
-							house[i]='G';
-						}else if(dp[i]==dp[i-1]+B[i]) {
-							house[i]='B';
-						}
-					}else if(house[i-1]=='B') {
-						dp[i]=Math.min(dp[i-1]+R[i], dp[i-1]+G[i]);
-						if(dp[i]==dp[i-1]+R[i]) {
-							house[i]='R';
-						}else if(dp[i]==dp[i-1]+G[i]) {
-							house[i]='G';
-						}
+			}
+			//DP 실행(1행부터 탐색 시작)
+			for(int i=1;i<n;i++) {
+				for(int j=0;j<n;j++) {
+					if(j==0) {
+						dp[i][j]=Math.min(dp[i-1][1], dp[i-1][2])+dp[i][j];
+					}else if(j==1) {
+						dp[i][j]=Math.min(dp[i-1][0], dp[i-1][2])+dp[i][j];
 					}else {
-						dp[i]=Math.min(dp[i-1]+B[i], dp[i-1]+R[i]);
-						if(dp[i]==dp[i-1]+B[i]) {
-							house[i]='B';
-						}else if(dp[i]==dp[i-1]+R[i]) {
-							house[i]='R';
-						}
+						dp[i][j]=Math.min(dp[i-1][0], dp[i-1][1])+dp[i][j];
 					}
 				}
-				if(min>dp[n-1]) {
-					min=dp[n-1];
-				}
-				k++;
 			}
-			System.out.println(min);
+			System.out.println(Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]));
 		}
 	}
 }
