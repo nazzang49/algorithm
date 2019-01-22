85ebd18d5a2eb5c17ef3eb3146076729f10ee815
nazzang49 <34739974+nazzang49@users.noreply.github.com>
2019-01-22 14:37:42 +0900

    https://www.acmicpc.net/problem/2743

---
+++ B_2743.java
@@ -0,0 +1,15 @@
+package test_190122;
+
+import java.util.Scanner;
+
+//단어가 입력되면 단어의 길이 출력하기
+public class B_2743 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		System.out.println(str.length());	
+	}
+}
