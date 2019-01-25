ad11c84a84e9724b181b6bbbacfc80f6758e4b35
nazzang49 <34739974+nazzang49@users.noreply.github.com>
2019-01-25 20:25:27 +0900

    https://www.acmicpc.net/problem/4889

--- B_4889.java
+++ B_4889.java
@@ -1,66 +1,72 @@
-package test_181203;
+package test_190125;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
-//괄호의 배열이 주어질 때 해당 배열이 안정성 판단 및 안정화 횟수 구하는 프로그램 만들기
+//괄호가 포함된 특정 문자열이 주어질 때, 이 문자열이 안정적인 문자열인지 판별하는 프로그램 작성하기
 public class B_4889 {
 
-	//안정화 횟수
-	static int cnt = 0;
-	static ArrayList<Integer> answer;
-	
-	//괄호 배열 안정성 여부 판단 메소드
-	public static void VPS(Stack<Character> vps, String str) {
-		//첫 문자는 무조건 삽입
-		vps.push(str.charAt(0));
-		for(int i=1;i<str.length();i++) {
+	public static int result(String str) {
+		Stack<Character> st = new Stack<>();
+		
+		for(int i=0;i<str.length();i++) {
 			if(str.charAt(i)=='{') {
-				vps.push('{');
+				st.push('{');
 			}else if(str.charAt(i)=='}') {
-				vps.pop();
-			}
-		}
-		int left = 0;
-		int right = 0;
-		//스택이 비어 있지 않다면
-		if(!vps.isEmpty()) {
-			for(int i=0;i<vps.size();i++) {
-				if(vps.get(i)=='}') {
-					left++;
+				if(!st.isEmpty()&&st.peek()=='{') {
+					st.pop();
 				}else {
-					right++;
+					st.push('}');
 				}
 			}
 		}
-		cnt=left*2+right/2;
-		answer.add(cnt);
-		cnt=0;
+		
+		int open = 0;
+		int close = 0;
+		//스택에 들어있는 여는 괄호, 닫는 괄호의 각 갯수 탐색
+		for(int i=0;i<st.size();i++) {
+			if(st.get(i)=='{') {
+				open++;
+			}else {
+				close++;
+			}
+		}
+		
+		if(open%2==0) {
+			open/=2;
+		}else {
+			open=open/2+1;
+		}
+		
+		if(close%2==0) {
+			close/=2;
+		}else {
+			close=close/2+1;
+		}
+		
+		return open+close;
 	}
 	
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		answer = new ArrayList<>();
 		
+		//정답 저장 배열
+		ArrayList<String> ans = new ArrayList<>();
+		int k = 1;
 		while(true) {
 			String str = sc.next();
-			//하이픈 입력 시 그간의 테스트 케이스별 정답 출력 후 종료
 			if(str.charAt(0)=='-') {
-				for(int i=0;i<answer.size();i++) {
-					System.out.println(answer.get(i));
+				//정답 출력 및 종료
+				for(int i=0;i<ans.size();i++) {
+					System.out.println(ans.get(i));
 				}
-				System.exit(0);
+				break;
 			}
-			//문자열의 길이는 2000이하이며 항상 짝수
-			if(str.length()<=2000&&str.length()%2==0) {
-				Stack<Character> vps = new Stack<>();
-				VPS(vps, str);
-			}else {
-				System.out.println("입력 범위를 확인하세요.");
-				System.exit(0);
-			}
+			int tmp = result(str);
+			ans.add(k+". "+tmp);
+			k++;
 		}
 	}
 }
